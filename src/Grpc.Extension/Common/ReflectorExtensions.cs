e00f8884e27663834091cf4ffdefd7efa4bee78f
yilei <87119538@qq.com>
2019-07-27 10:00:13 +0800

    1.清理过期方法，减轻历史负担 2.ReflectorExtensions加入自定义错误

--- src/Grpc.Extension/Common/ReflectorExtensions.cs
+++ src/Grpc.Extension/Common/ReflectorExtensions.cs
@@ -8,31 +8,72 @@ namespace Grpc.Extension.Common
 {
     public static class ReflectorExtensions
     {
+        /// <summary>
+        /// 获取属性值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="name"></param>
+        /// <param name="bindingFlags"></param>
+        /// <returns></returns>
         public static T GetPropertyValue<T>(this object obj, string name, BindingFlags bindingFlags) where T : class
         {
             var chProperty = obj.GetType().GetTypeInfo().GetProperty(name, bindingFlags);
+            if (chProperty == null) throw new InvalidOperationException($"Cannot locate property {name}");
             var chReflector = chProperty.GetReflector();
             var value = chReflector.GetValue(obj) as T;
 
             return value;
         }
 
+        /// <summary>
+        /// 获取字段值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="name"></param>
+        /// <param name="bindingFlags"></param>
+        /// <returns></returns>
         public static Tuple<T, FieldInfo> GetFieldValue<T>(this object obj, string name, BindingFlags bindingFlags) where T : class
         {
             var chField = obj.GetType().GetTypeInfo().GetField(name, bindingFlags);
+            if (chField == null) throw new InvalidOperationException($"Cannot locate field {name}");
             var chReflector = chField.GetReflector();
             var value = chReflector.GetValue(obj) as T;
 
             return Tuple.Create(value, chField);
         }
 
+        /// <summary>
+        /// 获取字段值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type"></param>
+        /// <param name="name"></param>
+        /// <param name="bindingFlags"></param>
+        /// <returns></returns>
         public static T GetFieldValue<T>(this Type type, string name, BindingFlags bindingFlags) where T : class
         {
             var chField = type.GetTypeInfo().GetField(name, bindingFlags);
+            if (chField == null) throw new InvalidOperationException($"Cannot locate field {name}");
             var chReflector = chField.GetReflector();
             var value = chReflector.GetValue(null) as T;
 
             return value;
+        }
+
+        /// <summary>
+        /// 获取方法
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="name"></param>
+        /// <param name="bindingFlags"></param>
+        /// <returns></returns>
+        public static MethodInfo GetMethodInfo(this Type type, string name, BindingFlags bindingFlags)
+        {
+            var method = type.GetMethod(name, bindingFlags);
+            if(method==null) throw new InvalidOperationException($"Cannot locate method {name}");
+            return method;
         }
     }
 }
